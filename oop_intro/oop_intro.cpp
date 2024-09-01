84782a0a2b431d2fde2d18d23bf8639afd790e7e
Vishnu Venugopal <vishnuvgn05@gmail.com>
2024-09-01 08:38:36 -0400

    changed readme, added oop_intro

---
+++ oop_intro/oop_intro.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+class Vehicle {
+    public:
+        int wheelNum;
+        std::string brand;
+        std::string model;
+
+        std::string getVehicleInfo(void) {
+            return brand + " " + model + " with " + std::to_string(wheelNum) + " wheels";
+        }
+        
+};
+
+
+int main(void) {
+    Vehicle car1;
+    car1.brand = "Toyota";
+    car1.model = "Supra";
+    car1.wheelNum = 4;
+    std::string vehicleInfo = car1.getVehicleInfo();
+    std::cout << vehicleInfo;
+    return 0;
+}
